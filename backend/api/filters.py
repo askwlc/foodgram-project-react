f9bd26e19c1b183da25dc1947b11ee3220f827a0
Никита Шинков <shinkovn@yandex.ru>
2023-06-13 19:31:47 +0400

    fix favorites

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -23,12 +23,12 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value == 1 and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(in_favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value == 1 and self.request.user.is_authenticated:
-            return queryset.filter(shoppingcarts__user=self.request.user)
+            return queryset.filter(cart_recipe__user=self.request.user)
         return queryset
 
 
